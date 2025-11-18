27efe6a2914b6c2f80d1c7c9b6574234adc13b25
nightmare <jastinmax888@gmail.com>
2025-11-18 09:04:03 +0200

    fix:adjust route to include activities detai

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import { healthCheck, getHello, postHello } from "../controllers/testController.
 import { register, login } from "../controllers/authController.js";
 import { getEquipment, getEquipmentById } from "../controllers/equipmentController.js";
 import { getMaterials } from "../controllers/materialController.js";
-import { getActivities, getActivitiesByType, createActivity } from "../controllers/activityController.js";
+import { getActivities, getActivitiesByType, createActivity, updateActivity, addCustomReason, getActivityDetailsOptions } from "../controllers/activityController.js";
 import { startOperation, stopOperation, getCurrentOperation, getOperations, getOperationById } from "../controllers/operationController.js";
 import { auth } from "../middleware/auth.js";
 
@@ -26,7 +26,10 @@ routes.get("/materials", auth, getMaterials);
 // Activity endpoints (reference data)
 routes.get("/activities", auth, getActivities);
 routes.get("/activities/type/:activityType", auth, getActivitiesByType);
+routes.get("/activities/:id/details", auth, getActivityDetailsOptions);
 routes.post("/activities", auth, createActivity);
+routes.put("/activities/:id", auth, updateActivity);
+routes.post("/activities/:id/custom-reason", auth, addCustomReason);
 
 // Operation endpoints (operational logs)
 routes.post("/operations/start", auth, startOperation);
